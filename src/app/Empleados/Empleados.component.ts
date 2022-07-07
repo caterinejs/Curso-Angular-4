779f2b2b9d2f2c8d46bfc59a1dbc0c763c71d058
caterinejs <cate-104@hotmail.com>
2022-07-07 11:38:04 -0500

    nueva version

--- src/app/Empleados/Empleados.component.ts
+++ src/app/Empleados/Empleados.component.ts
@@ -11,18 +11,35 @@ export class EmpleadosComponent{
     public titulo = 'Componente de Empleados'
     public empleado: Empleado;
     public trabajadores:Array<Empleado>;
+    public trabajador_externo: boolean;
+    public color:string;
+    public color_seleccionado:string
 
     constructor(){
-        this.empleado = new Empleado('Sofia', 6, 'cocinero', true);
+        this.empleado = new Empleado('Sofia', 6, 'niña', true);
         this.trabajadores = [
-            new Empleado('Pablo', 31, 'cocinero', true),
-            new Empleado('Milo', 3, 'cocinero', true),
-            new Empleado('Annie', 1, 'cocinero', false)
+            new Empleado('Pablo', 31, 'coordinador', true),
+            new Empleado('Milo', 3, 'perro', true),
+            new Empleado('Annie', 1, 'perro', false)
         ];
+        this.trabajador_externo= false;
+        this.color= 'red';
+        this.color_seleccionado= '#808080'
+        
     }
 
     ngOnInit(){
 
 console.log(this.empleado);
     }
+
+cambiarExterno(valor)
+{
+    this.trabajador_externo = valor;
+}
+
+logColorSeleccionado(){
+    console.log(this.color_seleccionado);
+
+}
 }
